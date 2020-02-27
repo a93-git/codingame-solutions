457e122916b850ef2c2af7f2d61cb52f61cd7299
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-27 22:56:14 +0530

    Create rev-clash2.py

---
+++ clash/reverse/rev-clash2.py
@@ -0,0 +1,14 @@
+""" Find the remainder when 2nd number is divided by the first one"""
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n1, n2 = [int(i) for i in input().split()]
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+
+print(n2%n1)
