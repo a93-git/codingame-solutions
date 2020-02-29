ae72ce3de20f993bce40236a6c233c76a9273985
a93-git <42908045+a93-git@users.noreply.github.com>
2020-02-29 13:20:12 +0530

    Create rev-clash8.py

---
+++ clash/reverse/rev-clash8.py
@@ -0,0 +1,11 @@
+""" Print the decimal representation of a hex number """
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+number = input()
+
+print(int(number, 16))
